19fcc088a03e353926a175395099c7ad69b7aeea
sferraro <sferraro@sfnet>
2018-03-19 00:14:44 -0300

    anda maso

--- pruebamax30100/pruebamax30100.ino
+++ pruebamax30100/pruebamax30100.ino
@@ -35,11 +35,15 @@ MAX30100 sensor;
 PulseOximeter ox;
 
 uint32_t tsLastReport = 0;
-int thresholdt = 50;
-int thresholdu = 60;
-
+int thresholdt = 48;
+int thresholdu = 50;
+int tread0=100;
+int tch=0;
+int tchv=0;
 /* create event group */
 EventGroupHandle_t eg;
+//creo el manejador para el semáforo como variable global
+SemaphoreHandle_t xSemaphore = NULL;
 
 // Callback (registered below) fired when a pulse is detected
 void onBeatDetected()
@@ -52,7 +56,7 @@ void setup()
     Serial.begin(115200);
     Serial.print("Initializing MAX30100..");
 
-    if (!sensor.begin()) {
+    if (!ox.begin()) {
         Serial.print("FAILED: ");
 
         uint8_t partId = sensor.getPartId();
@@ -74,49 +78,55 @@ void setup()
     sensor.setMode(MAX30100_MODE_SPO2_HR);
     Serial.println("done.");
 
-    Serial.print("Configuring LEDs biases to 50mA..");
-    sensor.setLedsCurrent(MAX30100_LED_CURR_50MA, MAX30100_LED_CURR_50MA);
-    Serial.println("done.");
+    //Serial.print("Configuring LEDs biases to 50mA..");
+    //sensor.setLedsCurrent(MAX30100_LED_CURR_50MA, MAX30100_LED_CURR_50MA);
+    //Serial.println("done.");
+    //delay(300);
+    //Serial.print("Lowering the current to 7.6mA..");
+    //sensor.setLedsCurrent(MAX30100_LED_CURR_7_6MA, MAX30100_LED_CURR_7_6MA);
+    //Serial.println("done.");
 
     delay(1000);
 
-    Serial.print("Lowering the current to 7.6mA..");
-    sensor.setLedsCurrent(MAX30100_LED_CURR_7_6MA, MAX30100_LED_CURR_7_6MA);
-    Serial.println("done.");
+    
+    //uint32_t tsTempSampStart = millis();
+    //Serial.print("Sampling die temperature..");
+    //sensor.startTemperatureSampling();
+    //while(!sensor.isTemperatureReady()) {
+    //    if (millis() - tsTempSampStart > 1000) {
+    //        Serial.println("ERROR: timeout");
+    //        // Stop here
+    //        for(;;);
+    //    }
+    //}
 
-    uint32_t tsTempSampStart = millis();
-    Serial.print("Sampling die temperature..");
-    sensor.startTemperatureSampling();
-    while(!sensor.isTemperatureReady()) {
-        if (millis() - tsTempSampStart > 1000) {
-            Serial.println("ERROR: timeout");
-            // Stop here
-            for(;;);
-        }
-    }
+    //float temperature = sensor.retrieveTemperature();
+    //Serial.print("done, temp=");
+    //Serial.print(temperature);
+    //Serial.println("C");
 
-    float temperature = sensor.retrieveTemperature();
-    Serial.print("done, temp=");
-    Serial.print(temperature);
-    Serial.println("C");
-
-    delay(1000);
+    //delay(1000);
 
     Serial.print("Shutting down..");
     sensor.shutdown();
     Serial.println("done.");
+   
+    //xSemaphore = xSemaphoreCreateBinary();
 
     eg = xEventGroupCreate();
+    EventBits_t xbit = xEventGroupClearBits(eg,TASK_1_BIT);
         // The default current for the IR LED is 50mA and it could be changed
     //   by uncommenting the following line. Check MAX30100_Registers.h for all the
     //   available options.
-    //pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
-    //pox.setIRLedCurrent(MAX30100_LED_CURR_24MA);
+    //ox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+    ox.setIRLedCurrent(MAX30100_LED_CURR_24MA);
 
     // Register a callback for the beat detection
     ox.setOnBeatDetectedCallback(onBeatDetected);
+    delay(1000);
+    touchAttachInterrupt(T0, gotTouch,thresholdt);
     printf("\n ESP32 Touch Interrupt \n");
-    touchAttachInterrupt(T0, gotTouch, threshold);
+    printf("Fin SETUP.\n");
 }
 
           
@@ -124,14 +134,15 @@ void loop()
 {
     // Make sure to call update as fast as possible
     /* wait forever until event bit of task 1 is set */
-    EventBits_t xbit = xEventGroupWaitBits(eg, TASK_1_BIT, pdTRUE, pdTRUE, portMAX_DELAY);
-    Serial.print("Loop has even bit: ");
-    Serial.println(xbit);
+    //if(xSemaphoreTake(xSemaphore,portMAX_DELAY) == pdTRUE){
+    EventBits_t xbit = xEventGroupWaitBits(eg, TASK_1_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+    //Serial.print("Loop has even bit: ");
+    //Serial.println(xbit);
     ox.update();
 
     // Asynchronously dump heart rate and oxidation levels to the serial
     // For both, a value of 0 means "invalid"
-    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
+    if(millis() - tsLastReport > REPORTING_PERIOD_MS){
         Serial.print("Heart rate:");
         Serial.print(ox.getHeartRate());
         Serial.print("bpm / SpO2:");
@@ -140,27 +151,52 @@ void loop()
 
         tsLastReport = millis();
     }
-    if (touchRead(T0) > thresholdu) {
-      Serial.println(touchRead(T0));  // get value using T0
-      uxBits = xEventGroupClearBits(eg,TASK_1_BIT);
-      delay(500);
-      touchAttachInterrupt(T0, gotTouch, delay(500));
-      delay(500);
+        tread0=touchRead(T0);
+    if(tread0 > thresholdu){
+      tchv++;
+      Serial.println("tchv++:");
+      Serial.println(tchv);
+      Serial.println(tread0);  // get value using T0    
+     }else{
+      tchv=0;
      }
-    
+    if(tchv>4){
+      Serial.println("tchv:");
+      Serial.println(tchv);
+      xbit = xEventGroupClearBits(eg,TASK_1_BIT);
+      Serial.print("Shutting down..");
+      sensor.shutdown();
+      Serial.println("done.");
+      tchv=0;
+      touchAttachInterrupt(T0, gotTouch,thresholdt);
+      delay(1000);
+    }
+  //}
 }
 
 void gotTouch(){
-  Serial.println("Touched\n");
-  BaseType_t xHigherPriorityTaskWoken;
-  Serial.print("Resuming normal operation..");
-  sensor.resume();
-  delay(500);
-  Serial.println("done.");
-  sensor.resetFifo();
-  printf("\n ESP32 Event TASK_1_BIT \n");
-  xEventGroupSetBitsFromISR(eg,TASK_1_BIT, &xHigherPriorityTaskWoken);
-  printf("\n ESP32 Touch Interrupt\n");
-  touchAttachInterrupt(T0, gotTouch, 10);
+  tch++;
+  if (tch == 1){
+   touchAttachInterrupt(T0, gotTouch, 0);
+   Serial.println("ISR Touched\n");
+   BaseType_t xHigherPriorityTaskWoken;
+   Serial.print("ISR Resuming normal operation..");
+   sensor.resume();
+   delay(500);
+   Serial.println("ISR done.");
+   //ox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+   ox.setIRLedCurrent(MAX30100_LED_CURR_24MA);
+   // Register a callback for the beat detection
+   ox.setOnBeatDetectedCallback(onBeatDetected);
+   sensor.resetFifo();
+   Serial.print("ISR ESP32 Event TASK_1_BIT\n");
+    //xSemaphoreGiveFromISR(xSemaphore, NULL);
+   xEventGroupSetBitsFromISR(eg,TASK_1_BIT, &xHigherPriorityTaskWoken);
+   Serial.print("ISR ESP32 Touch Interrupt\n");
+   tch=0;
+  }else{
+    Serial.print("ISR ESP32 Touch Interrupt duplicado");
+  }
+  
 }
 
